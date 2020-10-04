e3ee52a94c34ad9894c3fd77f3a8a9cfe60d731b
Brayden Curtis <braydencurtis@gmail.com>
2020-10-04 16:56:13 -0700

    Added JS that assigns default tab to URL hash

--- js/custom.js
+++ js/custom.js
@@ -239,3 +239,11 @@ if ($(".count-down").length) {
         padZeroes: true
     });
 }
+
+/* Assigns Services Tab to URL # */
+$(document).ready(function() {
+    if (location.hash !== '') $('a[href="' + location.hash + '"]').tab('show');
+    return $('a[data-toggle="tab"]').on('shown', function(e) {
+      return location.hash = $(e.target).attr('href').substr(1);
+    });
+});
